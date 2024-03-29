8f25207c131f632e4ffaf2007587642c320e524d
Herb Sneed <hmsneed@gmail.com>
2024-03-29 12:46:26 -0400

    mongodb atlas setup

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/us-chronicle');
+mongoose.connect('mongodb+srv://hmsneed79:Sneed2628191931@us-chronicle-cluster.lkumjq8.mongodb.net/?retryWrites=true&w=majority');
 
 module.exports = mongoose.connection;
